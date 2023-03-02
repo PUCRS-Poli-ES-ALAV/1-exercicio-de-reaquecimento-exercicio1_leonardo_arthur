1149a14777f31b3101bc96da8e62faa564ce70b4
leonardolforner <79606429+leonardolforner@users.noreply.github.com>
2023-03-02 17:55:14 -0300

    ex3 feito

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,10 +1,17 @@
 package br.pucrs.alav;
 
 public class Recursion {
-	public static int ex1(int n1, int n2){
-    if (n1 == 1){
-      return n2;
+    public static int ex1(int n1, int n2){
+        if (n1 == 1){
+        return n2;
     }
-    return n2 + ex1(n1 - 1, n2);
+        return n2 + ex1(n1 - 1, n2);
+  }
+
+  public static int ex3(int max){
+    if (max == 1){
+      return 1;
+    }
+    return max + 1/ex3(max - 1);
   }
 }
