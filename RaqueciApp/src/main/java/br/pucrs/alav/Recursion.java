ec3789f8358ea0913daac726b6515a52ccc3489a
leonardolforner <79606429+leonardolforner@users.noreply.github.com>
2023-03-09 18:51:39 -0300

    mais ex feitos

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,29 +1,104 @@
-package br.pucrs.alav;
+import java.util.ArrayList;
 
 public class Recursion {
-    public static int ex1(int n1, int n2){
-        if (n1 == 1){
-        return n2;
+    public static int fact(int n){
+        if (n == 1){
+            return 1;
+        }
+        if (n == 0){
+            return 1;
+        }
+        if (n < 0){
+            // throw exception
+        }
+        return n* fact(n-1);
     }
-        return n2 + ex1(n1 - 1, n2);
-  }
 
-  public static int ex3(int max){
-    if (max == 1){
-      return 1;
+    public static int somt(int n){
+        if(n < 0){
+            // throw exception
+        }
+        if(n == 0){
+            return 0;
+        }
+        return n + somt(n - 1);
     }
-    return max + 1/ex3(max - 1);
-  }
 
-  public static boolean ex8(String st, int c1, int length){
-    if (c1 == length){
-      return true;
+
+    public static int fibn(int n){
+        if(n <= 0){
+            //throw exception
+        }
+        if(n==1||n==2){
+            return 1;
+        }
+        return 0;
+        //Não terminado !!!!!!!!!
     }
-    if( st.charAt(c1) != st.charAt(length)){
-      return false;
+
+    public static int somtInt(int n1, int n2){
+        if(n2 >n1){
+            //throw exception
+        }
+
+        if(n1 == n2){
+            return n1;
+        }
+        return n1 + somtInt(n1 + 1, n2);
     }
-    else{
-      return ex8(st, c1 + 1, length -1);
+    /**
+     * public static boolean isPal(String s){
+        if (){
+
+        }
+        return true;
     }
-  }
+     * 
+     */
+    
+
+    public static int nroDigit(int n){
+        if( n>=0 && n < 10){
+            return 1;
+        }
+        if( n<0 && n > -10){
+            return 1;
+        }
+
+        return 1 + nroDigit(n/10);
+    }
+    //6
+    /**
+     * public static boolean is2(int n){
+        if(n%2 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static String convBase2(int n){
+        if (n == 0) {
+            return "0";
+        }
+        if (n == 1){
+            return "1";
+        }
+
+        return "" + convBase2(n/2);
+    }
+     * 
+     */
+    // 7
+    public static int smtList(ArrayList<Integer> list, int size){
+        if(size == 0){
+            return list.get(size);
+        }
+        
+        return list.get(size) + smtList(list, size-1);
+        
+    }
+
+
 }
